3aa63dbead0fb3bb848d93dbf76e5db2357da224
Viacheslav <19wiltor69@gmail.com>
2023-12-15 16:47:55 +0200

    Fixed mongoose

--- controllers/auth.js
+++ controllers/auth.js
@@ -47,7 +47,7 @@ const login = async (req, res) => {
   const payload = {
     id: auth._id,
   };
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "30d" });
   await Auth.findOneAndUpdate(auth._id, { token });
 
   res.json({
