d55e72bc7ee453c80cc7a13166ccc90d257ed53c
Illia12august <121337310+Illia12august@users.noreply.github.com>
2023-12-16 22:36:02 +0300

    commit to auth.js

--- controllers/user.js
+++ controllers/user.js
@@ -2,18 +2,14 @@ import { ctrlWrapper } from "../helpers/index.js";
 import { User } from "../models/user.js";
 
 const updateUser = async (req, res) => {
-  console.log(req.auth);
   const userId = req.auth._id;
+  const user = User.findById(userId);
 
+  console.log(user.weight);
   await User.findByIdAndUpdate(userId, {
-    goal: "lose weight",
-    gender: "male",
-    age: 0,
-    weight: 0,
-    height: 0,
     ...req.body,
   });
-  res.json({ message: "updated" });
+  res.json({ message: `updated` });
 };
 export default {
   updateUser: ctrlWrapper(updateUser),
