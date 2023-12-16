0fc70347077e810b3e46e94303da13d60170d82a
Viacheslav <19wiltor69@gmail.com>
2023-12-16 13:35:17 +0200

    Fixed

--- routes/api/recommendFoods.js
+++ routes/api/recommendFoods.js
@@ -2,8 +2,8 @@ import express from "express";
 import ctrl from "../../controllers/recommendFoods.js";
 
 const router = express.Router();
-const jsonParser = express.json();
+// const jsonParser = express.json();
 
-router.get("/api/recommended-food", jsonParser, ctrl.getFoods);
+router.get("/api/recommended-food", ctrl.getFoods);
 
 export default router;
