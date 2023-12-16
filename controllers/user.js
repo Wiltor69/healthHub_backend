3e2800de3dbfceac47dbbce1b16eebaef42dcec8
Illia12august <121337310+Illia12august@users.noreply.github.com>
2023-12-16 20:38:57 +0300

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -4,16 +4,18 @@ const { User } = require("../models/user");
 const updateUser = async (req, res) => {
   console.log(req.auth);
   const userId = req.auth._id;
-
+  const user = User.findById(userId);
+  console.log(user);
   await User.findByIdAndUpdate(userId, {
     goal: "lose weight",
     gender: "male",
     age: 0,
     weight: 0,
     height: 0,
+    ...user,
     ...req.body,
   });
-  res.json({ message: "updated" });
+  res.json({ message: `${user}` });
 };
 module.exports = {
   updateUser: ctrlWrapper(updateUser),
