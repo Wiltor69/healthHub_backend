f8399504fd32aefee9d63953d6c43ff1980bf6b0
Viacheslav <19wiltor69@gmail.com>
2023-12-21 20:48:59 +0200

    Fixed refactor

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,17 +2,17 @@ import express from "express";
 import { validateBody } from "../../middlewares/index.js";
 import { updateSchema } from "../../models/user.js";
 import authenticate from "../../middlewares/authenticate.js";
-import userControllers from "../../controllers/user.js";
+import ctrl from "../../controllers/user.js";
 
 const router = express.Router();
 
-router.get("/current", authenticate, userControllers.getUserCurrent);
+router.get("/current", authenticate, ctrl.getUserCurrent);
 
 router.put(
   "/update",
   authenticate,
   validateBody(updateSchema),
-  userControllers.updateUser
+  ctrl.updateUser
 );
 
 export default router;
